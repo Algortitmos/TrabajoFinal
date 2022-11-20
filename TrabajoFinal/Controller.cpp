9cf70769c0be36ec1dbf6ce594cf015e51dab032
RauVR <u201714049@upc.edu.pe>
2022-11-20 16:12:13 -0500

    Nuevas Opciones

--- TrabajoFinal/Controller.cpp
+++ TrabajoFinal/Controller.cpp
@@ -37,19 +37,19 @@ void Controller::menu()
 
 
 		cout << " Bienvenido a Cambio seguro, Elija una opcion : "<< endl << endl;
-		cout << " 1. Comprar o vender dolares" << endl;
-		cout << " 2. Ver transacciones." << endl;
-		cout << " 3. Registrar Usuario." << endl;
-		cout << " 4. Listar Usuarios." << endl;
-		cout << " 5. Cargar datos de usuarios guardados.." << endl;
-		cout << " 6. Registrar Reclamo" << endl;
-		cout << " 7. Mostrar Reclamos" << endl;
-		cout << " 8. Salir." << endl << endl;
-		
-		cout << " Ingrese una opcion : ";
+		//cout << " 1. Comprar o vender dolares" << endl;
+		//cout << " 2. Ver transacciones." << endl;
+		//cout << " 3. Registrar Usuario." << endl;
+		//cout << " 4. Listar Usuarios." << endl;
+		//cout << " 5. Cargar datos de usuarios guardados.." << endl;
+		//cout << " 6. Registrar Reclamo" << endl;
+		//cout << " 7. Mostrar Reclamos" << endl;
 
-		
-		cin >> opcion;
+		cout << " 1. Iniciar sesion "<< endl;
+		cout << " 2. Registrarse " << endl;
+		cout << " 3. Salir." << endl << endl;
+		cout << " Ingrese opcion : "; cin >> opcion;
+
 
 		switch (opcion)
 		{
@@ -63,62 +63,15 @@ void Controller::menu()
 
 		case 2:
 			system("cls");
-			cout << "Aqui estan las Transacciones"<<endl;
-			
-			system("pause");
-			system("cls");
-			PantInicio();
-			menu();
-
-
-		case 3:
 
 			registroUsuario();
-			//system("pause");
+
 			system("pause");
 			system("cls");
 			PantInicio();
 			menu();
 
-		case 4:
-
-			Console::Clear();
-			cout << " -------------LISTA DE USUARIOS -------------";
-			cout << endl << endl;
-			mostrarListaUsuarios();
-			system("pause");
-			system("cls");
-			PantInicio();
-			menu();
-
-		case 5:
-			system("cls");
-			cargarUsuarios();
-			system("pause");
-			system("cls");
-			PantInicio();
-			menu();
-			
-
-		case 6:
-			system("cls");
-			registrarReclamo();
-			system("pause");
-			system("cls");
-			PantInicio();
-			menu();
-			
-
-		case 7:
-
-			system("cls");
-			mostrarReclamos();
-			system("pause");
-			system("cls");
-			PantInicio();
-			menu();
-			
-		case 8:
+		case 3:
 			exit(1);
 			break;
 
@@ -138,6 +91,8 @@ void Controller::registroUsuario() {
 	string correo;
 	string numero;
 
+	int opcionUser;
+
 	cout << "  ---------------- REGISTRO DE USUARIO ----------------" << endl << endl;
 
 	cout << "  Ingrese su nombre : "; cin.ignore(); getline(cin, nombre);
@@ -151,15 +106,31 @@ void Controller::registroUsuario() {
 
 	listaUsuarios.push_back(u);
 
-	
 	cant = u.cantElemenList;
 	ListTemp = u.listaAux;
 
-	u.operacion();
-	//u.getTransaccion().getResultado();
-	u.guardarDatosUsuario();
+	//u.operacion();
+	//u.guardarDatosUsuario();
 	grabarUsuarios();
-	//u.mostrarDatosUsuario();
+
+	cout << " Elija una opcion : " << endl << endl;
+	cout << " 1. Realizar transaccion."<<endl;
+	cout << " 2. Realizar un reclamo." << endl;
+	cout << " 3. Cerrar Sesion." << endl << endl;
+
+	cout << " Ingrese opcion : "; cin >> opcionUser;
+
+	switch (opcionUser)
+	{
+	case 1:
+		u.operacion();
+	case 2: 
+		u.realizar_GuardarReclamo();
+	case 3:
+		exit(1);
+	default:
+		break;
+	}
 
 
 }
@@ -219,7 +190,7 @@ void Controller::grabarUsuarios() {
 			//	archivo << "      --------------------------------" << endl << endl;
 			//}
 
-
+			cout << " Ud se ha registrado de manera exitosa";
 		}
 
 	}
@@ -230,7 +201,7 @@ void Controller::grabarUsuarios() {
 
 	archivo.close();
 
-	cout << "Grabacion exitosa";
+	
 }
 
 
